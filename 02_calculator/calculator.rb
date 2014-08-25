4956026db1a4982f00b3fcb73f3ee80017e542a9
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-25 14:36:45 -0400

    Calculator methods implemented

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -11,3 +11,14 @@ def sum(array)
     sum += value
   end
 end
+
+def multiply(array)
+  array.inject(1) do |product, value|
+    product = product * value
+  end
+end
+
+#From http://stackoverflow.com/questions/2434503/ruby-factorial-function
+def factorial(number)
+  (1..number).inject(:*) || 1
+end
