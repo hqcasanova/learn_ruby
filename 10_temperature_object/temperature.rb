4f96f865b0d4688a5755570349a17664c4e36a62
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-26 09:56:06 -0400

    Temperature, Celsius and Fahrenheit classes implemented

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -6,6 +6,14 @@ class Temperature
     @celsius = hash[:c]
   end
 
+  def self.in_fahrenheit(temp)
+    new({:f => temp})
+  end
+
+  def self.in_celsius(temp)
+    new({:c => temp})
+  end
+
   def to_fahrenheit
     return @fahrenheit if @fahrenheit
     (@celsius * 1.8 + 32).ceil
@@ -14,5 +22,17 @@ class Temperature
   def to_celsius
     return @celsius if @celsius
     ((@fahrenheit.to_f - 32.0) / 1.8).ceil
+  end
+end
+
+class Celsius < Temperature
+  def initialize(temp)
+    super({:c => temp})
+  end
+end
+
+class Fahrenheit < Temperature
+  def initialize(temp)
+    super({:f => temp})
   end
 end
